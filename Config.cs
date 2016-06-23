1c78acd82be3fbd336c075b98db0e7bb2ac28fb7
Jewsus <hismoses@hotmail.com>
2016-06-23 23:48:09 +1000

    API 1.23

---
+++ Config.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace RenamePlayer
+{
+    public class Config : Dictionary<string, string>
+    {
+
+        public bool LoadConfig(string configFile)
+        {
+            string[] file;
+            if (File.Exists(configFile))
+            {
+                file = File.ReadAllLines(configFile);
+            } // if
+            else
+            {
+                return false;
+            } // else
+
+            foreach (var t in file.Where(t => !t.StartsWith("//")
+                                             && !t.StartsWith("#"))
+                                                 .Where(t => t.Split('=').Length == 2))
+            {
+                Add(t.Split('=')[0].ToLower(), t.Split('=')[1]);
+            } // for
+
+            return CheckKeys();
+        } // Load
+
+
+        private bool CheckKeys()
+        {
+            var enableWordReplace = false;
+
+            foreach (var pair in this)
+            {
+                switch (pair.Key)
+                {
+                    case "enablewordreplace":
+                        if (bool.TryParse(pair.Value, out enableWordReplace))
+                        {
+                            enableWordReplace = true;
+                        }
+                        break;
+                } // switch
+
+            } // foreach
+
+            return enableWordReplace;
+        } // CheckKeys
+
+    } // Settings
+
+} // TerrariaIRC
---
+++ RenamePlayer.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Threading;
+using Terraria;
+using TShockAPI;
+using System.Text.RegularExpressions;
+using TerrariaApi.Server;
+
+namespace RenamePlayer
+{
+    [ApiVersion(1, 23)]
+    public class RenamePlayer : TerrariaPlugin
+    {
+        private TSPlayer _player;
+        private string _newName;
+        private bool _nameChanged = false;
+        private static string configFile = Path.Combine(TShock.SavePath, "RenamePlayer.cfg");
+        public static Config _config = new Config();
+        public static bool _replaceWords = false;
+
+        public override void Initialize()
+        {
+            ServerApi.Hooks.ServerJoin.Register(this, OnJoin);
+            ServerApi.Hooks.NetGreetPlayer.Register(this, OnGreetPlayer);
+
+            if (_config.LoadConfig(configFile))
+            {
+                try
+                {
+                    _replaceWords = bool.Parse(_config["enablewordreplace"]);
+                }
+                catch (KeyNotFoundException)
+                {
+                    Console.WriteLine("! Unable to parse 'enableWordReplace' from config file: " + configFile);
+                    Thread.Sleep(5000);
+                }
+            }
+            else
+            {
+                Console.WriteLine("! Config failed to load, skipping name replacement.");
+                Thread.Sleep(5000);
+            }
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.NetGreetPlayer.Deregister(this, OnGreetPlayer);
+                ServerApi.Hooks.ServerConnect.Deregister(this, OnJoin);
+                base.Dispose(disposing);
+            }
+        }
+        void OnJoin(int playerId, HandledEventArgs eventArgs)
+        {
+            try
+            {
+                _player = TShock.Players[playerId];
+                ConvertEncoding();
+                ReplaceWords();
+
+                if (_player.Name != _newName)
+                {
+                    TShock.Utils.Broadcast(string.Format("Player '{0}' has been renamed to '{1}'.", _player.Name, _newName),
+                                            Color.IndianRed);
+                    TShock.Log.Info(string.Format("Player '{0}' has been renamed to '{1}'.", _player.Name, _newName));
+
+                    _player.TPlayer.name = _newName;
+                    _nameChanged = true;
+                }
+            }
+            catch (Exception exception)
+            {
+                TShock.Log.Error(string.Format("! Error: {0}: {1}", exception.Message, exception.StackTrace));
+            }
+        }
+        void OnGreetPlayer(GreetPlayerEventArgs args)
+        //void OnGreetPlayer(int playerId, HandledEventArgs eventArgs)
+        {
+            if (_nameChanged)
+            {
+                _player.SendMessage("Warning: Your name had invalid characters, it has been changed to: " + _newName,
+                                     Color.OrangeRed);
+            }
+
+        }
+        private void ConvertEncoding()
+        {
+            byte[] byteArray = Encoding.Unicode.GetBytes(_player.Name);
+            byte[] asciiArray = Encoding.Convert(Encoding.Unicode, Encoding.ASCII, byteArray);
+
+            _newName = Encoding.ASCII.GetString(asciiArray);
+            if (_player.Name != _newName)
+            {
+                _newName = "guest_" + _player.Name.ToString().GetHashCode().ToString("x").Substring(0, 4);
+            }
+        }
+        private void ReplaceWords()
+        {
+
+            if (_replaceWords)
+            {
+                foreach (var entry in _config)
+                {
+                    if (_newName.ToLower().Contains(entry.Key.ToLower()))
+                    {
+                        _newName = Regex.Replace(_newName, entry.Key, entry.Value, RegexOptions.IgnoreCase);
+                        //            _newName = _newName.ToLower().Replace( entry.Key.ToLower(), entry.Value );
+                    }
+                }
+            }
+
+        }
+        public override string Name
+        {
+            get { return "RenamePlayer"; }
+        }
+        public override string Author
+        {
+            get { return "Scavenger, _Jon"; }
+        }
+        public override string Description
+        {
+            get { return "Renames players with Invalid Characters"; }
+        }
+        public override Version Version
+        {
+            get { return new Version(1, 0, 3, 0); }
+        }
+        public RenamePlayer(Main game) : base(game)
+        {
+            Order = -11;
+        }
+    }
+}
